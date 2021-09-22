d8e2d8723c6954ae7fa099653981f9a953bf1935
Bruno Leitao Donatelli <bleitaod@everis.com>
2021-09-22 14:40:23 -0300

    integration with axios to get addresses

---
+++ acessibilidade-web-front-end-2/js/cep.js
@@ -0,0 +1,39 @@
+async function getCityData() {
+    const zipCodeNumber = document.getElementById('cep').value;
+    try {
+        const zipCodeInfo = await axios.get(`https://viacep.com.br/ws/${zipCodeNumber}/json/`);
+
+        const { bairro: neighbour, logradouro: address, localidade: city, uf: state } = zipCodeInfo.data;
+
+        new AddressBuilder()
+            .address(address)
+            .neighbour(neighbour)
+            .city(city)
+            .state(state);
+
+        console.log(zipCodeInfo);
+    } catch (err) {
+        console.error(err);
+    }
+}
+class AddressBuilder {
+    address(address) {
+        const neighbourValue = document.querySelectorAll('.contatoCampo-campoDesabilitado')[0];
+        neighbourValue.setAttribute('value', address);
+        return this;
+    }
+    neighbour(neighbourName) {
+        const addressValue = document.querySelectorAll('.contatoCampo-campoDesabilitado')[1];
+        addressValue.setAttribute('value', neighbourName);
+        return this;
+    }
+    city(cityName) {
+        const cityValue = document.querySelectorAll('.contatoCampo-campoDesabilitado')[2];
+        cityValue.setAttribute('value', cityName);
+        return this;
+    }
+    state(stateName) {
+        const stateValue = document.querySelectorAll('.contatoCampo-campoDesabilitado')[3];
+        stateValue.setAttribute('value', stateName);
+    }
+}
