a2f63d806f0c720a5aecaf106c84a78b56e17962
bruno donatelli <bruno.donatelli@etec.sp.gov.br>
2021-12-15 13:19:53 -0300

    removed modal from section and added aria label to modal

--- acessibilidade-web-front-end-1/js/dialog.js
+++ acessibilidade-web-front-end-1/js/dialog.js
@@ -1,18 +1,25 @@
 // Variáveis
+const conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+
 const btnAbreDialog = document.querySelector('#abreDialog');
 const dialog = document.querySelector('.dialogNewsletter');
 const dialogBody = document.querySelector('.dialogNewsletter-body');
 const dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
-
+btnAbreDialog.style.display = 'block';
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', () => {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.setAttribute('inert', '');
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.removeAttribute('inert');
+
+  btnAbreDialog.focus();
 }
 
 // Listeners
