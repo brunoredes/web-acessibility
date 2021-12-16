f29873069e4faa72039cbe311fb7f2061a4d7cac
bruno donatelli <bruno.donatelli@etec.sp.gov.br>
2021-12-16 10:13:28 -0300

    add new data attribute to carousel and making more acessible

--- acessibilidade-web-front-end-1/js/carousel.js
+++ acessibilidade-web-front-end-1/js/carousel.js
@@ -1,29 +1,32 @@
 const new0 = document.querySelector('#new0');
 const new1 = document.querySelector('#new1');
 const new2 = document.querySelector('#new2');
-
+const noticias = document.querySelectorAll('.listaDeArtigos-item')
 
 new0.style.display = 'block'
 const btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+const indicadorSlideAtual = document.createElement('span');
+indicadorSlideAtual.classList.add('escondeVisualmente');
+indicadorSlideAtual.textContent = "(Slide atual)";
+indicadorSlideAtual.id = 'escondeVisualmente';
 
 // Percorre todos os botoes controladores
 
 for (const button of btns) {
   button.addEventListener('click', () => {
+
     const dataSliderItem = button.getAttribute('data-sliderItem');
-    if (dataSliderItem === '0') {
-      new0.style.display = 'block';
-      new1.style.display = 'none';
-      new2.style.display = 'none';
-    } else if (dataSliderItem === '1') {
-      new0.style.display = 'none';
-      new1.style.display = 'block';
-      new2.style.display = 'none';
-    } else {
-      new0.style.display = 'none';
-      new1.style.display = 'none';
-      new2.style.display = 'block';
-    }
+    noticias.forEach(noticia => {
+      noticia.style.display = 'none';
+      const dataNoticia = noticia.getAttribute('data-noticia');
+      if (dataSliderItem === dataNoticia) {
+        noticia.style.display = 'block';
+      }
+    })
+
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
+    button.append(indicadorSlideAtual);
     // Remove classe 'ativo' dos outros botoes
     for (const buttonRemoveClass of btns) {
       buttonRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo');
